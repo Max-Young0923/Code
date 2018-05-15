3e744a37c17164e4d4e33ebe0cf785f025ffb30e
ricesgui <hwjavatoxg@163.com>
2018-05-15 19:07:22 +0800

    excu

---
+++ Pacific Trails Resort/pacific.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pacific Trails Resort</title>
+	<link rel="stylesheet" type="text/css" href="pacific.css">
+</head>
+<body>
+<h1 class="biaoti">Pacific Trails Resort</h1>
+<h2>We will be contacting you soon!</h2>
+Here is the information you entered:
+*myFName:<br/>
+*myLName:<br/>
+myEmail:<br/>
+myPhone:<br/>
+myComments:<br/>
+<a href="Reservations.html"><input type="submit" name="返回" value="返回"></a>
+<footer id="foot1">
+<span id="foot" >Copyright &copy; 2018 Pacific Trails Resort All Rights Reserved.</span>
+</footer>
+</body>
+</html>
